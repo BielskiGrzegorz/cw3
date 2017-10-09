cb0babdddfbaf38aca4a8e06dd4a181db1df3af6
Bielski Grzegorz <bielskigrzegorz@outlook.com>
2017-10-09 13:38:07 +0200

    zad3
    z tablicy

--- Zadanie/main.cpp
+++ Zadanie/main.cpp
@@ -7,34 +7,39 @@ using namespace std;
 struct Probka {
 double t;
 double x;
+Probka (double t1,double x1)
+{
+    t=t1;
+    x=x1;
+
+}
 };
 
 
-int main()
+type vector<Probka> dane;
+Dane wczytaj (string nazwa.pliku)
 {
+    Dane tablica;
+    ifstream plik(nazwa.pliku);
     string linia;
-    ifstream wyczt("sygnal.csv");
     while(getline(wyczt,linia))
     {
-        cout<<linia<<endl;\
-        double x,t;
-        stringstream ss(linia);
-        ss>>t;
-        ss.ignore();
-        ss>>x;
+        cout<<linia<<endl;
+        Probka probka1(t,x);
     }
+    plik.close();
+    return tablica;
 
-    /*
-    Probka probka1 = { 0.1, 0.2 };
-    vector<Probka> dane;
+}
+int main(int argc, char* argv[])
+{
+    if(argc !=2)
+    {
+        return -1;
 
-    Probka probka1(0.1, 0.2);
-    dane.push_back(probka1);
-    dane.push_back(Probka(0.2, 0.3));
-    cout << dane[0].first << ”, ” << dane[0].second;
-
-    */
-    wyczt.close();
+    }
+    cout<<argv[1]<<endl;
+    Dance dane = wczytaj(argv[1]);
 
     return 0;
 }
